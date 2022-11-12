9433edec6d57db469888e132da5c863dc727a701
yc <yecaofree@163.com>
2022-11-12 15:59:59 +0800

    modify pin

--- bmc_gd32f303/sub_devices/dev_power.c
+++ bmc_gd32f303/sub_devices/dev_power.c
@@ -14,9 +14,9 @@ const static GPIOConfig g_gpioConfig_power[] = {
     {GPIO_OUT_P5V_EN,       GPIOC, GPIO_PIN_9,  RCU_GPIOC, GPIO_MODE_OUT_PP, GPIO_OSPEED_10MHZ, 1},
 
     {GPIO_IN_R_GPIO0,       GPIOB, GPIO_PIN_15, RCU_GPIOB, GPIO_MODE_IN_FLOATING, GPIO_OSPEED_10MHZ, 0},
-    {GPIO_IN_P3V3_PWRGD,    GPIOD, GPIO_PIN_14, RCU_GPIOD, GPIO_MODE_IN_FLOATING, GPIO_OSPEED_10MHZ, 1},
-    {GPIO_IN_P5V_PWRGD,     GPIOC, GPIO_PIN_6,  RCU_GPIOC, GPIO_MODE_IN_FLOATING, GPIO_OSPEED_10MHZ, 1},
-    {GPIO_IN_P12V_PWRGD,    GPIOC, GPIO_PIN_8,  RCU_GPIOC, GPIO_MODE_IN_FLOATING, GPIO_OSPEED_10MHZ, 1},
+    {GPIO_IN_P12V_PWRGD,    GPIOD, GPIO_PIN_14, RCU_GPIOD, GPIO_MODE_IN_FLOATING, GPIO_OSPEED_10MHZ, 1},//modify pin num
+    {GPIO_IN_P5V_PWRGD,     GPIOC, GPIO_PIN_8,  RCU_GPIOC, GPIO_MODE_IN_FLOATING, GPIO_OSPEED_10MHZ, 1},//modify pin num
+    {GPIO_IN_P3V3_PWRGD,    GPIOC, GPIO_PIN_6,  RCU_GPIOC, GPIO_MODE_IN_FLOATING, GPIO_OSPEED_10MHZ, 1},//modify pin num
 };
 
 const GPIOConfig_Handler g_gpioConfigHandler_power = {
@@ -39,12 +39,13 @@ static SubDevice_Reading_T g_adcVal_power[ARRARY_SIZE(g_adcChannlConfig_power)];
 static const  SensorConfig g_sensor_power[] = {
     {ADC_CHANNEL_13,        SUB_DEVICE_SDR_P3V3,       "P3V3_AUX_M"},
     {ADC_CHANNEL_1,         SUB_DEVICE_SDR_P5V,        "P5V_M"},
-    {ADC_CHANNEL_2,         SUB_DEVICE_SDR_P3V3,       "VBAT_M"},
+    {ADC_CHANNEL_2,         SUB_DEVICE_SDR_VBAT,       "VBAT_M"},  //modify SUB_DEVICE_SDR_VBAT 3.0V
     {ADC_CHANNEL_5,         SUB_DEVICE_SDR_TEMP,       "WORKING_TEMP"},
     {ADC_CHANNEL_6,         SUB_DEVICE_SDR_P12V,       "P12V_M"},
     {ADC_CHANNEL_7,         SUB_DEVICE_SDR_P3V3,       "P3V3_M"},
 };
 static SubDevice_Reading_T g_sensorVal_power[ARRARY_SIZE(g_sensor_power)];
+
 const Dev_Handler g_devHandler_power = {
     .mode = SUB_DEVICE_MODE_POWER,
     .val = g_sensorVal_power,
@@ -58,10 +59,10 @@ static UINT32 CountPinPluseMs(BMC_GPIO_enum pin)
     #define GPIO_SCAN_PEROID 10
     uint32_t  lastMs = GetTickMs();
     uint32_t offsetMs;
-    if (!GPIO_isPinActive(GPIO_IN_R_GPIO0)){
+    if (!GPIO_isPinActive(pin)){
         return 0;
     }else{
-        while (GPIO_isPinActive(GPIO_IN_R_GPIO0))
+        while (GPIO_isPinActive(pin))
         {
             vTaskDelay(GPIO_SCAN_PEROID);
         }
@@ -73,14 +74,17 @@ static void DevTaskHandler(void *pArg)
 {
     while (1)
     {
-        vTaskDelay(5);
+        vTaskDelay(10);
         if (GPIO_isPinActive(GPIO_IN_R_GPIO0)){
-            
+            if(CountPinPluseMs(GPIO_IN_R_GPIO0)>90){
+            }
+            else{
+            }  
         }
         LOG_E("filename = %s, line = %d", __FILE__, __LINE__);
 
-        if (api_sensorGetValHuman(ADC_CHANNEL_13) > 3.0){
-            
-        }
+//        if (api_sensorGetValHuman(ADC_CHANNEL_13) > 3.0){
+//            
+//        }
     }
 }
